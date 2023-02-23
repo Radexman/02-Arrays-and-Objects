9185f0d5550c4d44a1e38af02f973191a29d2415
Radosław Siek <Borderlandsmaniak@gmail.com>
2023-02-23 11:47:55 +0100

    23.02

--- 05-object-literals/script.js
+++ 05-object-literals/script.js
@@ -1,46 +1,63 @@
-let x;
-const person = {
-	name: 'John Doe',
-	age: 30,
-	isAdmin: true,
-	address: {
-		street: '123 Main st',
-		city: 'Boston',
-		state: 'MA',
-	},
-	hobbies: ['coding', 'guitar', 'fishing'],
+// let x;
+// const person = {
+// 	name: 'John Doe',
+// 	age: 30,
+// 	isAdmin: true,
+// 	address: {
+// 		street: '123 Main st',
+// 		city: 'Boston',
+// 		state: 'MA',
+// 	},
+// 	hobbies: ['coding', 'guitar', 'fishing'],
+// };
+
+// x = person.name;
+// x = person['age'];
+// x = person.address.state;
+// person.hobbies.push('hiking');
+// x = person.hobbies;
+// person.name = 'Radosław Siek';
+// x = person.name;
+// person['isAdmin'] = false;
+// x = person;
+// delete person.age;
+// person.hasChildern = false;
+// person.greet = function () {
+// 	console.log(`Hello, my name is ${this.name}`);
+// };
+
+// person.greet();
+
+// const radek = {
+// 	'first name': 'Radosław',
+// 	'last name': 'Siek',
+// };
+
+// x = radek['first name'];
+
+// console.log(x);
+
+// const fruits = ['banana', 'orange', 'strawberry'];
+// const vegetables = ['carrot', 'potato', 'leek'];
+// const food = [...fruits, ...vegetables];
+// food.push('pizza');
+// food.reverse();
+
+// console.log(food);
+
+const house = {
+	family: 'Siek',
+	location: 'Koszęcin',
+	hasBasement: true,
+	occupants: ['Piotr', 'Katarzyna', 'Bartosz', 'Radosław'],
 };
 
-x = person.name;
-x = person['age'];
-x = person.address.state;
-person.hobbies.push('hiking');
-x = person.hobbies;
-person.name = 'Radosław Siek';
-x = person.name;
-person['isAdmin'] = false;
-x = person;
-delete person.age;
-person.hasChildern = false;
-person.greet = function () {
-	console.log(`Hello, my name is ${this.name}`);
+house.year = 2011;
+house.greeting = function () {
+	console.log(`${this.family} family lives in here.`);
 };
 
-person.greet();
+delete house.occupants[3];
 
-const radek = {
-	'first name': 'Radosław',
-	'last name': 'Siek',
-};
-
-x = radek['first name'];
-
-console.log(x);
-
-const fruits = ['banana', 'orange', 'strawberry'];
-const vegetables = ['carrot', 'potato', 'leek'];
-const food = [...fruits, ...vegetables];
-food.push('pizza');
-food.reverse();
-
-console.log(food);
+house.greeting();
+console.log(house.occupants[3]);
