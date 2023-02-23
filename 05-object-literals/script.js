6a296b55b292f26830f865f74a3d49c7461ae049
Radosław Siek <Borderlandsmaniak@gmail.com>
2023-02-23 11:28:50 +0100

    23.02

--- 05-object-literals/script.js
+++ 05-object-literals/script.js
@@ -1,5 +1,46 @@
+let x;
 const person = {
-	name: 'John',
+	name: 'John Doe',
 	age: 30,
-	location: 'Boston',
+	isAdmin: true,
+	address: {
+		street: '123 Main st',
+		city: 'Boston',
+		state: 'MA',
+	},
+	hobbies: ['coding', 'guitar', 'fishing'],
 };
+
+x = person.name;
+x = person['age'];
+x = person.address.state;
+person.hobbies.push('hiking');
+x = person.hobbies;
+person.name = 'Radosław Siek';
+x = person.name;
+person['isAdmin'] = false;
+x = person;
+delete person.age;
+person.hasChildern = false;
+person.greet = function () {
+	console.log(`Hello, my name is ${this.name}`);
+};
+
+person.greet();
+
+const radek = {
+	'first name': 'Radosław',
+	'last name': 'Siek',
+};
+
+x = radek['first name'];
+
+console.log(x);
+
+const fruits = ['banana', 'orange', 'strawberry'];
+const vegetables = ['carrot', 'potato', 'leek'];
+const food = [...fruits, ...vegetables];
+food.push('pizza');
+food.reverse();
+
+console.log(food);
